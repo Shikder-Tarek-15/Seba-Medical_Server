11c4608b1f11dcf708502cad142704d3830247c5
Shikder Tarek <shikdertarek2019@gmail.com>
2024-06-18 21:44:35 +0600

    Add delete function

--- index.js
+++ index.js
@@ -126,6 +126,13 @@ async function run() {
         res.send(result)
       })
 
+      app.delete("/participant_camp/:id", async(req, res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await participantCampCollection.deleteOne(query);
+        res.send(result)
+      })
+
       app.post('/participant/:email', async(req, res)=>{
         const email = req.params.email;
         const query = {participantEmail: email}
