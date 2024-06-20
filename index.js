ea4862eb40b52fb6bd00522ba51b034431fba332
Shikder Tarek <shikdertarek2019@gmail.com>
2024-06-20 21:33:28 +0530

    Update deploy element

--- index.js
+++ index.js
@@ -9,7 +9,15 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 
 //middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      // "http://localhost:5173",
+      "seba-medical.web.app",
+      "seba-medical.firebaseapp.com",
+    ]
+  })
+);
 app.use(express.json());
 
 
@@ -275,7 +283,7 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
